8295689791b8f3fe5c7871b7904c44683dc3d27e
João Orismar <orismarfh@gmail.com>
2017-07-11 16:07:36 -0300

    Fisrt Screen

---
+++ TopRioFm/ViewModels/BaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AudioForms.ViewModels
+{
+	public class BaseViewModel : INotifyPropertyChanged
+	{
+		//Assinatura da Interface
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		// Refactor - Deixar tudo dentro de um mesmo Método//Refactor - Diretiva CALLERMEMBERNAM
+		protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+		{
+			if (EqualityComparer<T>.Default.Equals(storage, value))
+			{
+				return false;
+			}
+			storage = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+	}
+}
---
+++ TopRioFm/ViewModels/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using AudioForms.ViewModels;
+using Plugin.MediaManager;
+using Plugin.MediaManager.Abstractions;
+using Xamarin.Forms;
+
+namespace Furacao2000
+{
+	public class MainViewModel : BaseViewModel
+	{
+		
+
+		private IPlaybackController PlaybackController => CrossMediaManager.Current.PlaybackController;
+
+		public Command PauseCommand { get; }
+		public Command PlayCommand { get; }
+		public Command LinkFaceCommand { get; }
+		public Command LinkInstaCommand { get; }
+
+		public MainViewModel ()
+		{
+			PauseCommand = new Command (ExecutePauseCommand);
+			PlayCommand = new Command (ExecutePlayCommand);
+			LinkFaceCommand = new Command (ExecuteLinkFaceCommand);
+			LinkInstaCommand = new Command (ExecuteLinkInstaCommand);
+			CrossMediaManager.Current.Play ("http://streaming32.hstbr.net:8334/live");
+		}
+
+		void ExecuteLinkInstaCommand (object obj)
+		{
+			Device.OpenUri (new Uri ("https://www.instagram.com/topriofm/"));
+		}
+
+		                  
+		void ExecuteLinkFaceCommand (object obj)
+		{
+			Device.OpenUri (new Uri ("https://www.facebook.com/toprio971fm/"));
+		}
+
+		void ExecutePlayCommand (object obj)
+		{
+			PlaybackController.Play ();
+		}
+
+		void ExecutePauseCommand ()
+		{
+			PlaybackController.Pause ();
+		}
+
+
+		
+	}
+}
